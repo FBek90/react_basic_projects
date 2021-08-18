3ecd1059bae560824c9f2ad07d117902f8d95ae9
FBek90 <f.f.rustamov@bk.ru>
2021-08-18 19:33:19 +0900

    basic

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -4,9 +4,17 @@ import logo from '../logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='cocktail db logo' className='log' />
+        </Link>
+        <ul className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure repellat
+        aperiam delectus non minima cumque sunt earum a ratione ullam dolor,
+        molestias quibusdam? Ipsam nulla rerum est eveniet. Magni earum ipsam
+        aspernatur explicabo, id, eum vero impedit autem velit illum laboriosam
+        cupiditate, provident laborum eligendi facere animi est quibusdam non.
+      </p>
+    </section>
   )
 }
 
